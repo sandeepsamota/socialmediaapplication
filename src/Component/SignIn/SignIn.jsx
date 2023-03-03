1da457f6fb6bc2978093a4bac6e076820551984d
Sandeep Samota <112926724+sandeepsamota@users.noreply.github.com>
2023-03-03 12:17:31 +0530

    Update SignIn.jsx

--- src/Component/SignIn/SignIn.jsx
+++ src/Component/SignIn/SignIn.jsx
@@ -82,7 +82,7 @@ function SignIn() {
             <input
               type="image"
               src={
-                "https://cdn-clklo.nitrocdn.com/MYSuIlnSDlfpMrNTtwCzrqpXpFLTEsHf/assets/static/optimized/rev-0bcc36b/wp-content/uploads/2019/12/logo-2.png"
+                "https://eplanetsoft.com/wp-content/uploads/2019/12/logo-2.png"
               }
               alt="logo"
             />
